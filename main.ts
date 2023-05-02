b6736b39c1dc048e6e725992ef83ec9573e32e9b
Xj00ubjj <131229899+Xj00ubjj@users.noreply.github.com>
2023-05-02 22:55:15 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showIcon(IconNames.Heart)
+music.playMelody("B A G F E E B A ", 150)
+music.playMelody("A G F F G A G F ", 150)
+music.playMelody("E - - - - - - - ", 150)
+basic.forever(function () {
+	
+})
