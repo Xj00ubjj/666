6f693e23655b7546272c683120202c8552e98bab
Xj00ubjj <131229899+Xj00ubjj@users.noreply.github.com>
2023-05-02 22:58:59 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 basic.showIcon(IconNames.Heart)
-music.playMelody("B A G F E E B A ", 150)
-music.playMelody("A G F F G A G F ", 150)
-music.playMelody("E - - - - - - - ", 150)
+music.playMelody("B A G F E E B A ", 170)
+music.playMelody("G F F G A G F E ", 170)
 basic.forever(function () {
 	
 })
